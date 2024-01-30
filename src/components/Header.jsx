bb6917ac0a5d5c704a68a4ca8d35754aa403a0dd
Ilya <ilia.podrezov2001@gmail.com>
2024-01-31 01:55:56 +0300

    fix3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom"
 export function Header(props) {
     return (
         <div className="header">
-            <NavLink to='/' className="header__btn header__all">Все котики</NavLink>
+            <NavLink to='/catpintrest' className="header__btn header__all">Все котики</NavLink>
             <NavLink to='/favorites' className="header__btn header__favorites">Любимые котики</NavLink>
             {/* <button className="header__btn header__all"><NavLink to='/'>Все котики</NavLink></button>
             <button className="header__btn header__favorites"><NavLink to='/favorites'>Любимые котики</NavLink></button> */}
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,6 @@
 import {  useDispatch, useSelector } from "react-redux";
 import { Cat } from "./Cat";
 import { getMoreCats } from "../state/cats-reducer";
-import { useState } from "react";
 export function Main(props) {
     const page = useSelector(state => state.page);
     const isFethcing = useSelector(state => state.isFethcing);
